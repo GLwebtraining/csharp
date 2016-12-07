b43e91fb316dc8ab6bb9591e7405368c3e172cb6
glwebtrainig <vadym.riznyk@globallogic.com>
2016-12-08 00:52:01 +0200

    Lesson3 initial

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            converToBinary(5);
+        }
+
+        static string converToBinary(int n)
+        {
+        }
+    }
+}
