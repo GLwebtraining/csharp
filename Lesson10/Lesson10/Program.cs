677951eb69e7886f848b5779ed78c0e725ca3e31
Vadim Reznik <vadim.reznik@gmail.com>
2017-01-23 11:41:05 +0200

    c sharp lessons 9-12

---
+++ Lesson10/Lesson10/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+namespace Lesson10
+{
+
+    public static class StringExtensions {
+        public static bool IsNotNull(this string s) {
+            //return !string.IsNullOrWhiteSpace(s);
+            return !s.Satisfies(string.IsNullOrWhiteSpace);
+        }
+
+        public static bool Satisfies(this string s, Func<string, bool> predicate) {
+            return predicate(s);
+        }
+    }
+
+    public static class EmunsExtensions {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> inp, Func<T, bool> predicate) {
+            foreach (var e in inp) {
+                if (predicate(e)) { yield return e; }
+            }
+        }
+    }
+
+    class Program
+    {   
+
+
+
+        public MathFunc F { get; set; }
+
+        // the same as
+        // public Func<int, int, int> F { get; set; }
+
+        public int Add(int a, int b)
+        {
+            Console.WriteLine("in add");
+            return a + b;
+
+        }
+
+        static bool isEven(int a) { return true; }
+
+        static void Main(string[] args)
+        {
+
+            var pr = new Program();
+            //pr.F = pr.Add;
+            var str = "asdfasdf";
+            str.IsNotNull();
+            pr.F += Substract;
+            pr.F += pr.Add;
+            
+            var result = pr.F(1, 2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            var l1 = new List<int>() { 1, 2, 3 };
+
+            var l2 = l1.Select(a => a * 2);
+
+            var allEvens = l1.Where(e => isEven(e));
+
+            foreach (var ae in allEvens)
+            {
+                Console.WriteLine(ae);
+            }
+
+            var allEvens2 = l1.Filter(e => isEven(e));
+
+            Func<int, bool> predicate = (e) => isEven(e);
+
+            Func<int, int, int> add = (a, b) => a + b;
+
+            var filtered = l1.Where(predicate);
+
+            Console.WriteLine(l2);
+            Console.ReadKey();
+
+        }
+
+        static int Substract(int a, int b)
+        {
+            Console.WriteLine("IN Substract");
+            return a - b;
+        }
+    }
+
+    public delegate int MathFunc(int a, int b);
+}
---
+++ Lesson11/Lesson11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String - it is a reference type !
+            string s = " ";
+            s += "1";
+            s += "2";
+            s += "3";
+
+            // each concatination do allocate memory
+            // and it is the same as s = "1" + "2" + "3"
+            // better use String.Formar("{0}{1}{2}", "1", "2", "3"); or string interpolation
+            // when it has lots of strings to contcatinate (endless) better use StringBuilder
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("1");
+            // ...
+            sb.ToString();
+
+            // " " - bad , String.Empty - good
+
+            // Array
+            byte[] bytes = new byte[10];
+            for (int i = 0; i < bytes.Length; i++) { }
+
+            byte[] src = new byte[10];
+
+            byte[] dest = new byte[20];
+
+            // Array.Copy(src, dest); - read about Copy arrays
+
+            byte[,,,] ndim = new byte[1,2,3,4];
+
+            byte[,] dim2 = new byte[1, 2];
+
+            byte[][] jagged = new byte[10][];
+
+
+
+            return;
+            Console.WriteLine(sb);
+            Console.ReadKey();
+        }
+    }
+}
